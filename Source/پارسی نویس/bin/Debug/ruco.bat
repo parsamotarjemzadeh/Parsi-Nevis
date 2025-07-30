1a14fe56570a2d190335c2b85d8fe50faa0a5398
Parsa Motarjemzadeh <85896624+parsamotarjemzadeh@users.noreply.github.com>
2025-07-30 18:53:51 +0330

    Add files via upload

---
+++ Source/پارسی نویس/bin/Debug/ruco.bat
@@ -0,0 +1,5 @@
+.\CSCC\csc.exe -out:.\TMP\P1.exe .\TMP\P1.cs
+copy .\TMP\P1.exe"" "C:\Users\Parsa kmz\Desktop\سلام سلام\tpfiles\P1.exe"
+del /f .\TMP\P1.cs
+del /f .\TMP\P1.exe
+start "" "C:\Users\Parsa kmz\Desktop\سلام سلام\tpfiles\P1.exe"
